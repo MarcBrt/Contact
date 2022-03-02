72a07842178c448349aae53a1c1edc7332de725f
Mael <dinocraftdidi@gmail.com>
2022-03-02 17:39:44 +0100

    Add adress

---
+++ src/main/java/fr/cnam/contact/entity/Adress.java
@@ -0,0 +1,75 @@
+package fr.cnam.contact.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Adress {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    private String label;
+    private String postalCode;
+    private String city;
+    private String adress;
+    private String additionalInformation;
+
+    @ManyToMany
+    private List<Contact> contactList = new ArrayList<Contact>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getAdditionalInformation() {
+        return additionalInformation;
+    }
+
+    public void setAdditionalInformation(String additionalInformation) {
+        this.additionalInformation = additionalInformation;
+    }
+
+    public List<Contact> getContactList() {
+        return contactList;
+    }
+
+}
