c9b8fb6c775c89463e356c707b4ea04e842bb44e
Mael <dinocraftdidi@gmail.com>
2022-03-02 17:26:07 +0100

    Add contact

---
+++ src/main/java/fr/cnam/contact/entity/Contact.java
@@ -0,0 +1,60 @@
+package fr.cnam.contact.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.ArrayList;
+
+@Entity
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    private String lastName;
+    private String firstName;
+    private List<Mail> mailList = new ArrayList<Mail>();
+    private List<Adress> adressList = new ArrayList<Adress>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public List<Mail> getMailList() {
+        return mailList;
+    }
+
+    public void setMailList(List<Mail> mailList) {
+        this.mailList = mailList;
+    }
+
+    public List<Adress> getAdressList() {
+        return adressList;
+    }
+
+    public void setAdressList(List<Adress> adressList) {
+        this.adressList = adressList;
+    }
+}
